845e2c8488c9d6e4d648327d897e8153b5844bc3
Kyle <klknudson@crimson.ua.edu>
2024-01-20 23:36:14 -0600

    why no workie

--- src/components/about/Education.jsx
+++ src/components/about/Education.jsx
@@ -21,7 +21,7 @@ const educationContent = [
         degree: 'DIPLOMA ',
         institute: 'LONG COUNTY HIGH SCHOOL',
         details: `Graduated with honors and was a member of Beta Club`,
-        logo: '/assets/img/logos/lc.jpg',
+        logo: '/assets/img/logos/pl.jpg',
     },
 ]
 
