4e5640d81998d0a6b85e5a24e3716809d94fb05d
Kyle <klknudson@crimson.ua.edu>
2024-01-20 20:59:44 -0600

    about logo changes

--- src/components/about/Education.jsx
+++ src/components/about/Education.jsx
@@ -6,18 +6,21 @@ const educationContent = [
         degree: 'MASTERS DEGREE',
         institute: 'THE UNIVERSITY OF ALABAMA',
         details: `  Specializing in Management Information Systems with a concentration in Solutions Delivery`,
+        logo: `/assets/img/logos/UA.png`,
     },
     {
         year: 'CURRENT',
         degree: 'BACHELORS DEGREE',
         institute: 'THE UNIVERSITY OF ALABAMA',
         details: `Majoring in Management Information Systems in the Accelerated Masters Program`,
+        logo: `/assets/img/logos/UA.png`,
     },
     {
         year: '2019',
         degree: 'DIPLOMA ',
         institute: 'LONG COUNTY HIGH SCHOOL',
         details: `Graduated with honors and was a member of Beta Club`,
+        logo: `/assets/img/logos/LC.jpg`,
     },
 ]
 
@@ -27,7 +30,7 @@ const Education = () => {
             {educationContent.map((val, i) => (
                 <li key={i}>
                     <div className='icon'>
-                        <i className='fa fa-briefcase'></i>
+                        <img src={val.logo} alt={`${val.institute} Logo`} />
                     </div>
                     <span className='time open-sans-font text-uppercase'>
                         {val.year}
--- src/components/about/Experience.jsx
+++ src/components/about/Experience.jsx
@@ -6,18 +6,21 @@ const experienceContent = [
         position: ' Programming Teaching Assistant',
         compnayName: 'UA MIS',
         details: `  Facilitate instruction in C# programming, providing technical evaluations and support to over 100 students each semester`,
+        logo: `/assets/img/logos/UA.png`,
     },
     {
         year: '2023 - 2023',
         position: ' Business Systems Analyst',
         compnayName: 'Protective Life',
         details: `Spearheaded system requirement compilation and communication, efficiently facilitating the transition of over 500,000 policies`,
+        logo: `/assets/img/logos/PL.png`,
     },
     {
         year: '2020 - 2023',
         position: 'Assistant Area Manager',
         compnayName: 'Dairy Queen',
         details: `Supported General Managers by overseeing operations, facilitating streamlined processes, and achieving performance metrics`,
+        logo: `/assets/img/logos/DQ.png`,
     },
 ]
 
@@ -27,7 +30,7 @@ const Experience = () => {
             {experienceContent.map((val, i) => (
                 <li key={i}>
                     <div className='icon'>
-                        <i className='fa fa-briefcase'></i>
+                        <img src={val.logo} alt={`${val.companyName} Logo`} />
                     </div>
                     <span className='time open-sans-font text-uppercase'>
                         {val.year}
